3c58a8d8b0a68aeee94469c86c536531cdedbfd7
dev-with-no-username <43436172+dev-with-no-username@users.noreply.github.com>
2024-10-28 09:58:06 +0100

    chore: remove commented code

--- main.go
+++ main.go
@@ -19,42 +19,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// const signCertPem = `-----BEGIN CERTIFICATE-----
-// MIIDBzCCAnCgAwIBAgIJAIJ/XyRx/DG0MA0GCSqGSIb3DQEBCwUAMIGZMQswCQYD
-// VQQGEwJOTDEVMBMGA1UECAwMWnVpZC1Ib2xsYW5kMRIwEAYDVQQHDAlSb3R0ZXJk
-// YW0xEjAQBgNVBAoMCVVuaWNvZGVyczELMAkGA1UECwwCSVQxGjAYBgNVBAMMEUpl
-// cm9lbiBCb2JiZWxkaWprMSIwIAYJKoZIhvcNAQkBFhNqZXJvZW5AdW5pY29kZXJz
-// Lm5sMCAXDTE3MDkxNzExMjkzNloYDzMwMTcwMTE4MTEyOTM2WjCBmTELMAkGA1UE
-// BhMCTkwxFTATBgNVBAgMDFp1aWQtSG9sbGFuZDESMBAGA1UEBwwJUm90dGVyZGFt
-// MRIwEAYDVQQKDAlVbmljb2RlcnMxCzAJBgNVBAsMAklUMRowGAYDVQQDDBFKZXJv
-// ZW4gQm9iYmVsZGlqazEiMCAGCSqGSIb3DQEJARYTamVyb2VuQHVuaWNvZGVycy5u
-// bDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAmrvrZiUZZ/nSmFKMsQXg5slY
-// TQjj7nuenczt7KGPVuGA8nNOqiGktf+yep5h2r87jPvVjVXjJVjOTKx9HMhaFECH
-// KHKV72iQhlw4fXa8iB1EDeGuwP+pTpRWlzurQ/YMxvemNJVcGMfTE42X5Bgqh6Dv
-// kddRTAeeqQDBD6+5VPsCAwEAAaNTMFEwHQYDVR0OBBYEFETizi2bTLRMIknQXWDR
-// nQ59xI99MB8GA1UdIwQYMBaAFETizi2bTLRMIknQXWDRnQ59xI99MA8GA1UdEwEB
-// /wQFMAMBAf8wDQYJKoZIhvcNAQELBQADgYEAkOHdI9f4I1rd7DjOXnT6IJl/4mIQ
-// kkaeZkjcsgdZAeW154vjDEr8sIdq+W15huWJKZkqwhn1sJLqSOlEhaYbJJNHVKc9
-// ZH5r6ujfc336AtjrjCL3OYHQQj05isKm9ii5IL/i+rlZ5xro/dJ91jnjqNVQPvso
-// oA4h5BVsLZPIYto=
-// -----END CERTIFICATE-----`
-
-// const signKeyPem = `-----BEGIN RSA PRIVATE KEY-----
-// MIICWwIBAAKBgQCau+tmJRln+dKYUoyxBeDmyVhNCOPue56dzO3soY9W4YDyc06q
-// IaS1/7J6nmHavzuM+9WNVeMlWM5MrH0cyFoUQIcocpXvaJCGXDh9dryIHUQN4a7A
-// /6lOlFaXO6tD9gzG96Y0lVwYx9MTjZfkGCqHoO+R11FMB56pAMEPr7lU+wIDAQAB
-// AoGADPlKsILV0YEB5mGtiD488DzbmYHwUpOs5gBDxr55HUjFHg8K/nrZq6Tn2x4i
-// iEvWe2i2LCaSaBQ9H/KqftpRqxWld2/uLbdml7kbPh0+57/jsuZZs3jlN76HPMTr
-// uYcfG2UiU/wVTcWjQLURDotdI6HLH2Y9MeJhybctywDKWaECQQDNejmEUybbg0qW
-// 2KT5u9OykUpRSlV3yoGlEuL2VXl1w5dUMa3rw0yE4f7ouWCthWoiCn7dcPIaZeFf
-// 5CoshsKrAkEAwMenQppKsLk62m8F4365mPxV/Lo+ODg4JR7uuy3kFcGvRyGML/FS
-// TB5NI+DoTmGEOZVmZeLEoeeSnO0B52Q28QJAXFJcYW4S+XImI1y301VnKsZJA/lI
-// KYidc5Pm0hNZfWYiKjwgDtwzF0mLhPk1zQEyzJS2p7xFq0K3XqRfpp3t/QJACW77
-// sVephgJabev25s4BuQnID2jxuICPxsk/t2skeSgUMq/ik0oE0/K7paDQ3V0KQmMc
-// MqopIx8Y3pL+f9s4kQJADWxxuF+Rb7FliXL761oa2rZHo4eciey2rPhJIU/9jpCc
-// xLqE5nXC5oIUTbuSK+b/poFFrtjKUFgxf0a/W2Ktsw==
-// -----END RSA PRIVATE KEY-----`
-
 const outputFilename = "output.pdf"
 
 func main() {
@@ -211,42 +175,3 @@ func createCertificate2(privateKey *rsa.PrivateKey) (x509.Certificate, [][]*x509
 
 	return *rootCertificate, certificateChain, nil
 }
-
-// func createCertificate() (*rsa.PrivateKey, x509.Certificate, [][]*x509.Certificate, error) {
-// 	certificate_data_block, _ := pem.Decode([]byte(signCertPem))
-// 	if certificate_data_block == nil {
-// 		log.Println("failed to parse PEM block containing the certificate")
-// 		return nil, x509.Certificate{}, [][]*x509.Certificate{}, nil
-// 	}
-
-// 	certificate, err := x509.ParseCertificate(certificate_data_block.Bytes)
-// 	if err != nil {
-// 		log.Printf("%s", err.Error())
-// 		return nil, x509.Certificate{}, [][]*x509.Certificate{}, err
-// 	}
-
-// 	key_data_block, _ := pem.Decode([]byte(signKeyPem))
-// 	if key_data_block == nil {
-// 		log.Println("failed to parse PEM block containing the private key")
-// 		return nil, x509.Certificate{}, [][]*x509.Certificate{}, err
-// 	}
-
-// 	privateKey, err := x509.ParsePKCS1PrivateKey(key_data_block.Bytes)
-// 	if err != nil {
-// 		log.Printf("%s", err.Error())
-// 		return nil, x509.Certificate{}, [][]*x509.Certificate{}, err
-// 	}
-
-// 	roots := x509.NewCertPool()
-// 	roots.AddCert(certificate)
-
-// 	certificateChain, err := certificate.Verify(x509.VerifyOptions{
-// 		Roots: roots,
-// 	})
-// 	if err != nil {
-// 		log.Printf("Failed to create certificate chain: %v\n", err)
-// 		return privateKey, x509.Certificate{}, [][]*x509.Certificate{}, err
-// 	}
-
-// 	return privateKey, *certificate, certificateChain, nil
-// }
